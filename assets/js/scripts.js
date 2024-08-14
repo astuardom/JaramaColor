f2d9c809819cb5538b8def8c7772bbf70f6ed927
astuardom <73834220+astuardom@users.noreply.github.com>
2024-08-14 15:43:09 -0400

    subiendo7

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -69,3 +69,9 @@ document.addEventListener('click', (e) => {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
